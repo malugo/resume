e9c1dd007cecf7d13a7672d1393ef1914f356180
Avril Hsu <malugo350@gmail.com>
2015-02-28 01:01:24 +0800

    動態調整
    添加Css效果

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
   console.log('----open-----')
+
   //開啟menu
-  $(".menu-icon").mouseover(function(){
+  $(".inner.right").mouseenter(function(){
   	console.log('icon')
-  	$(".menu-icon ul").toggle()
+  	$(this).find("ul").show("slow")
+  }).mouseleave(function(){
+  	$(this).find("ul").hide("slow")
   })
 
+  //照片放大
+  // $('.about-me img,.company img').mouseover(function(){
 
+  // });
+	$("img[alt=avrilhsu]").mouseover(function(){
+		console.log('ee')
+		$(this).css("-webkit-tranform","scale(0.8,1.8)")
+	});
 
-
-
+  console.log('----close-----')
 });
